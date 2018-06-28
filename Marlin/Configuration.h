285e3460d75b69bca06e7e5d32b747993f85dfe8
Thorsten Gilfert <thorsten.gilfert@googlemail.com>
2018-06-28 18:52:19 +0200

    Z axis steps changed for lead screws

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -567,7 +567,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.34, 80.34, 2560, 105 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.34, 80.34, 400, 105 }
 
 /**
  * Default Max Feed Rate (mm/s)
