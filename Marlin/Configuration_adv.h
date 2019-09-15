19083e1ef531e264d06ad77c1d0ca989987a9c40
Thorsten Gilfert <thorsten.gilfert@googlemail.com>
2019-09-15 12:37:18 +0200

    Limit max. PWM for 12V fan on 24V.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -302,8 +302,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-//#define FAN_MIN_PWM 50
-//#define FAN_MAX_PWM 128
+#define FAN_MIN_PWM 30
+#define FAN_MAX_PWM 128
 
 /**
  * FAST PWM FAN Settings
